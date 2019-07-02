8472d30b1f5f389382f517825907b2ace91ffa89
greendb16 <greendb16@gmail.com>
2019-07-02 14:55:15 -0400

    Fixed search

---
+++ src/Recruiter.java
@@ -0,0 +1,19 @@
+//import java.util.ArrayList;
+//
+//public class Recruiter {
+//    public String recruiter(String input, ArrayList<Resume> resList) {
+//        int m = 0;
+//        for (Resume r : resList) {
+//            for (Skill s : r.getSkList()) {
+//                if (input.equals(s.getSkill())) {
+//
+//                    m++;
+//                    return (r.getDetails().getName());
+//                }
+//            }
+//        }
+//        if (m == 0) {
+//            return "There are no matches for that skill.  Please try again.";
+//        }
+//    }
+//}
